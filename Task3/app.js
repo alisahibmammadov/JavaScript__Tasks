9ed3fbdc58d20cf5e4d259860067b80873e790e3
alisahibmammadov <ali.yek.10@mail.ru>
2023-10-06 16:50:15 +0400

    msg

--- Task3/app.js
+++ Task3/app.js
@@ -76,6 +76,7 @@ function isEven (num){
 //     return num % 2 === 0 ? true : false
 // }
 // let isEven = num => return num % 2 === 0 ? true : false
+// let isEven = num => return num % 2 === 0 
 
 
 console.log(isEven(3));// false
