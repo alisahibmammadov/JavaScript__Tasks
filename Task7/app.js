5991359f571009fb46d194844d02c31269bbc41b
alisahibmammadov <ali.yek.10@mail.ru>
2023-10-16 13:27:29 +0400

    msg

--- Task7/app.js
+++ Task7/app.js
@@ -1,20 +1,30 @@
 let obj = {
-  name: "Alisahib",
-  surname: "Mammadov",
+  name: "Əlisahib",
+  surname: "Məmmədov",
   age: 25,
   birthday: {
     day: "24",
-    month: "November",
+    month: "Noyabr",
     year: 1998,
+    getFullBirthday: () =>
+      `${obj.birthday.day} ${obj.birthday.month} ${obj.birthday.year}`,
   },
   address: {
-    coutry: "Azerbaijan",
-    city: "Baku",
-    home: "Khirdalan",
+    coutry: "Azərbaycan",
+    city: "Bakı",
+    home: "Xırdalan",
     postcode: 909,
+    getFullAddress: () =>
+      `Mən ${obj.address.coutry}nın ${obj.address.city} şəhərinin ${obj.address.home} qəsəbəsində yaşayıram`,
   },
   contact: {
     phoneNumber: "+994505555555",
     email: "alisahibmammadov98@gmail.com",
   },
+  getFullInfo: () =>
+    `Salam. Mən ${obj.name} ${obj.surname}. ${obj.birthday.day}.${obj.birthday.month}.${obj.birthday.year}-ci ildə dünyaya gəlmişəm. Hal hazırda ${obj.address.city} şəhərinin ${obj.address.home} qəsəbəsində yaşayıram.Ana vətənim ${obj.address.coutry}-dır`
+  
 };
+console.log(obj.birthday.getFullBirthday());
+console.log(obj.address.getFullAddress());
+console.log(obj.getFullInfo());
