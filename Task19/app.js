8876f58a3c8bf69aa0de8261c2219ba7745232b9
alisahibmammadov <ali.yek.10@mail.ru>
2023-10-19 23:08:21 +0400

    msg

---
+++ Task19/app.js
@@ -0,0 +1,4 @@
+function splitSentence(sentence) {
+  return console.log(sentence.split(" "));
+}
+splitSentence("I'm  Front-End developer in Azerbaijan");
---
+++ Task20/app.js
@@ -0,0 +1,6 @@
+function removeCharacterFromString(str,str2){
+    return console.log(str.replace(str2,""));
+}
+
+
+removeCharacterFromString("Alion","A")
