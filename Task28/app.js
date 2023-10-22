2dc51128b8953e53983f32618b3249eb4c0bff9b
alisahibmammadov <ali.yek.10@mail.ru>
2023-10-22 19:02:32 +0400

    msg

---
+++ Task28/app.js
@@ -0,0 +1,23 @@
+// const arr = []
+// // arr.length = 5
+// while(arr.length !== 5){
+//     arr.push(+prompt('Eded daxil edin '))
+// }
+// console.log(arr);
+//!!!!!!!!!!!!!!!!!!!
+// const arr = [1,2,5]
+// let hasil = 1
+// for (const val of arr) {
+//     hasil*=val
+// }
+// console.log(hasil);
+// !!!!!!!!!!!!!!!!!!!!!
+// let hasil = 1;
+// function multiply(...arg) {
+//   for (const val of arg) {
+//     hasil *= val;
+//   }
+//   console.log(hasil);
+// }
+// multiply(1, 2, 4, 5);
+// !!!!!!!!!!!!!!!!!!!!!!!!!!
