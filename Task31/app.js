bbd4f53a52b70c4a730fa62f5529fa59314a247a
alisahibmammadov <ali.yek.10@mail.ru>
2023-11-05 22:42:57 +0400

    msg

---
+++ Task31/app.js
@@ -0,0 +1,26 @@
+// const numbers = [-1, 2, 5, 7, 7, -3, 7, -6, 4, -3];
+
+// console.log(numbers.reduce((a, b) => console.log(a - b), 0));
+
+// const negativeNumbers = (arr) => {
+//   return arr.filter((neg) => neg < 0).join();
+// };
+
+// console.log(negativeNumbers(numbers));
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let names = ["ali", "reza", "alireza", "muhammad", "coshqun", "eles"];
+
+// const longName = (arr) => {
+//   return arr.reduce((a, b) => {
+//     return a.length > b.length ? a : b;
+//   }, "");
+// };
+// console.log(longName(names));
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"];
+// const printWithIndex = (name) => {
+//   console.log(names.indexOf(name) + "-" + names[names.indexOf(name)]);
+// };
+// names.forEach(printWithIndex);
