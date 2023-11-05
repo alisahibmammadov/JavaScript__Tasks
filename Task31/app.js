d341c760b76a9c316f19391cd1b0b3dc42963ae9
alisahibmammadov <ali.yek.10@mail.ru>
2023-11-05 23:26:32 +0400

    msg

--- Task31/app.js
+++ Task31/app.js
@@ -42,19 +42,32 @@ let names = ["Sam", "Alan", "Bill", "Adam", "Anna", "George"];
 console.log(names.filter((elem) => elem[0] === "A"));
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-let numbers = [1, 4, 7, 8, 3, 5, 0, 2, 4, 6, 12, 15, 18, 46, 45, 48, 97, 84];
-let odd = 0;
-let even = 0;
-const countEvensAndOdds = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0 && arr[i] - 2 !== -2 ) {
-      even++;
-    //   console.log(even);
-    } else if (arr[i] % 2 === 1) {
-      odd++;
-    }
-  }
-  console.log("odd"+ " "+odd,"even"+" "+even);
-};
+// let numbers = [1, 4, 7, 8, 3, 5, 0, 2, 4, 6, 12, 15, 18, 46, 45, 48, 97, 84];
+// let odd = 0;
+// let even = 0;
+// const countEvensAndOdds = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0 && arr[i] - 2 !== -2 ) {
+//       even++;
+//     //   console.log(even);
+//     } else if (arr[i] % 2 === 1) {
+//       odd++;
+//     }
+//   }
+//   console.log("odd"+ " "+odd,"even"+" "+even);
+// };
 
-countEvensAndOdds(numbers); // evens: 10, odds: 7
+// countEvensAndOdds(numbers); // evens: 10, odds: 7
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+let guys = ["Ada Lovelace, 1915", "Nicola Tesla, 1856", "Alan Turing, 2012"];
+const sortByBirth = (arr) => {
+  arr.sort((a, b) => {
+    let c = parseInt(a.split(", ")[1]);
+    let d = parseInt(b.split(", ")[1]);
+    return c - d
+  });
+  return console.log(arr);
+};
+sortByBirth(guys)
+
