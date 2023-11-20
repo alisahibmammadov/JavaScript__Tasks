1df53aacacadf7ccf911fe3cc750c7167d2b985e
Alisahib Mammadov <101056016+alisahibmammadov@users.noreply.github.com>
2023-11-20 23:55:11 +0400

    Update app.js

--- Task10/app.js
+++ Task10/app.js
@@ -6,7 +6,7 @@ const existsAndTruthy = (obj, str) => {
   }else if(obj[str]){
     return console.log(true);
   }
-};
+}; 
 existsAndTruthy({a:1,b:2,c:3},'b') //true
 existsAndTruthy({x:'a',y:null,z:'c'},'y') //false
 existsAndTruthy({ x: "a", b: "b", z: undefined }, "z"); //false
